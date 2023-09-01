b96a8acc01e63d051cb09b4c8f2ae41887ff8a0f
EminMadadi <amin_madadi@hotmail.com>
2023-09-02 00:27:46 +0330

    Step 04 - Create new class 'Post' at 'blog.models'

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+
+    STATUS_CHOICES = (
+        (0, 'Draft'),
+        (1, 'Published'),
+    )
+
+    title = models.CharField(max_length=40)
+    text = models.TextField()
+    author = models.ForeignKey('auth.USER', on_delete=models.CASCADE)
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS_CHOICES, max_length=1)
+
+    def __str__(self):
+        return f'{self.title} | {self.author}'
