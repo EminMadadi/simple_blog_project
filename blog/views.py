b16ba2abd346c2adc04992d82f154a58fd8a2295
EminMadadi <amin_madadi@hotmail.com>
2023-09-03 21:54:46 +0330

    Step 15 - Modify 'post_list_view' view at 'blog.views' - Change '/blog/posts_list.html' to 'blog/posts_list.html'

--- blog/views.py
+++ blog/views.py
@@ -5,4 +5,4 @@ from .models import Post
 
 def post_list_view(request):
     posts_list = Post.objects.filter(status=Post.STATUS_CHOICES[1][0])
-    return render(request, '/blog/posts_list.html', {'posts_list': posts_list})
+    return render(request, 'blog/posts_list.html', {'posts_list': posts_list})
